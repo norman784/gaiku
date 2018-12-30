a547ac269d7b0ff9044c22f18541d1b6766ba5a1
norman784 <normanpaniagua@gmail.com>
2018-12-30 19:16:51 +0100

    bump

--- gaiku-common/src/lib.rs
+++ gaiku-common/src/lib.rs
@@ -1,3 +1,25 @@
 // TODO: Add common implementations to read file content and pass to the binary parser implementation
 // TODO: Check how amethyst does this
-pub trait FileFormat {}
+
+use std::fs::File;
+
+mod data;
+
+pub use self::{
+    data::Chunk,
+    data::Mesh,
+    data::Vector3,
+};
+
+pub trait Baker {
+    fn bake(chunks: &Chunk) -> Mesh;
+}
+
+pub trait Fileformat {
+    fn load(stream: &File) -> Vec<Chunk>;
+
+    fn read(file: &str) -> Vec<Chunk> {
+        let stream = &(File::open(file).unwrap());
+        Self::load(&stream)
+    }
+}
