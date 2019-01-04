e1131a1f6c2b86c99500b852cc7b5014cf2f9806
norman784 <normanpaniagua@gmail.com>
2019-01-04 12:46:24 +0100

    added vectors structs

--- gaiku-common/src/data/mod.rs
+++ gaiku-common/src/data/mod.rs
@@ -1,9 +1,57 @@
 mod chunk;
 mod mesh;
 
-pub type Vector2 = [f32; 2];
-pub type Vector3 = [f32; 3];
-pub type Vector4 = [f32; 4];
+#[derive(Debug, Copy, Clone)]
+pub struct Vector2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl From<[f32; 2]> for Vector2 {
+    fn from(value: [f32; 2]) -> Self {
+        Vector2 { x:  value[0], y: value[1] }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vector3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl From<[f32; 3]> for Vector3 {
+    fn from(value: [f32; 3]) -> Self {
+        Vector3 { x:  value[0], y: value[1], z: value[2] }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vector3i {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+impl From<[i32; 3]> for Vector3i {
+    fn from(value: [i32; 3]) -> Self {
+        Vector3i { x:  value[0], y: value[1], z: value[2] }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vector4 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+impl From<[f32; 4]> for Vector4 {
+    fn from(value: [f32; 4]) -> Self {
+        Vector4 { x:  value[0], y: value[1], z: value[2], w: value[3] }
+    }
+}
 
 pub use self::{
     chunk::Chunk,
