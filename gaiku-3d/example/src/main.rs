173387ef7f01e215bef1054b8b6e316e06cdb261
norman784 <normanpaniagua@gmail.com>
2019-01-04 15:52:26 +0100

    added measurement and new small gox file

--- gaiku-3d/example/src/main.rs
+++ gaiku-3d/example/src/main.rs
@@ -1,5 +1,7 @@
 extern crate gaiku_3d;
 
+use std::time::Instant;
+
 use gaiku_3d::{
     common::{
         Baker,
@@ -14,21 +16,33 @@ mod exporter;
 use crate::exporter::export;
 
 fn read(name: &str) -> std::io::Result<()> {
+    let now = Instant::now();
     let file = format!("{}/assets/{}.gox", env!("CARGO_MANIFEST_DIR"), name);
-    let chunks= GoxReader::read(&file);
+    let chunks = GoxReader::read(&file);
     let mut meshes = vec![];
 
-    for chunk in chunks {
-        let mesh  = VoxelBaker::bake(&chunk);
-        meshes.push((mesh, chunk.get_position()));
+    let reader_elapsed = now.elapsed().as_secs();
+    let now = Instant::now();
+
+    for chunk in chunks.iter() {
+        let mesh  = VoxelBaker::bake(chunk);
+        if let Some(mesh) = mesh {
+            meshes.push((mesh, chunk.get_position()));
+        }
     }
 
+    let baker_elapsed = now.elapsed().as_secs();
+    let now = Instant::now();
+
     export(meshes, name);
+
+    println!("<<{}>> Chunks: {} Reader: {} Baker: {} secs Export: {} secs", name, chunks.len(), reader_elapsed, baker_elapsed, now.elapsed().as_secs());
 
     Ok(())
 }
 
 fn main() -> std::io::Result<()> {
+    let _ = read("small_tree");
     let _ = read("terrain");
     let _ = read("planet");
 
