edbbcccb4157b09c2db0d64f5252782ba09b46f1
norman784 <normanpaniagua@gmail.com>
2020-09-22 18:44:40 +0200

    bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 pub use gaiku_common as common;
 
 pub mod bakers {
-  pub use gaiku_baker_heightmap::*;
-  pub use gaiku_baker_marching_cubes::*;
   pub use gaiku_baker_voxel::*;
 }
 
 pub mod formats {
   pub use gaiku_loader_gox::*;
-  pub use gaiku_loader_png::*;
 }
