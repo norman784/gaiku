ea9d80d1e75bf29ad7694d16a396c6ca6415a0ca
norman784 <normanpaniagua@gmail.com>
2021-01-16 14:55:26 +0100

    gaiku-common docs

--- gaiku-common/src/boxify.rs
+++ gaiku-common/src/boxify.rs
@@ -1,8 +1,10 @@
+/// Define a component that contains a position.
 pub trait Positionable {
   fn with_position(position: [f32; 3]) -> Self;
   fn position(&self) -> [f32; 3];
 }
 
+/// Define a component that contains a 3d size.
 pub trait Sizable {
   fn with_size(width: u16, height: u16, depth: u16) -> Self;
   fn width(&self) -> u16;
@@ -10,6 +12,7 @@ pub trait Sizable {
   fn depth(&self) -> u16;
 }
 
+/// Define a component that is `Sizable` and `Positionable`, also a initializer
 pub trait Boxify: Positionable + Sizable {
   fn new(position: [f32; 3], width: u16, height: u16, depth: u16) -> Self;
 }
--- gaiku-common/src/chunk.rs
+++ gaiku-common/src/chunk.rs
@@ -4,12 +4,14 @@ mod sparse_chunk;
 pub use chunk::Chunk;
 pub use sparse_chunk::SparseChunk;
 
+/// Base common denominator across all the chunk implementations used.
 pub trait Chunkify<T> {
   fn is_air(&self, x: usize, y: usize, z: usize) -> bool;
   fn get(&self, x: usize, y: usize, z: usize) -> T;
   fn set(&mut self, x: usize, y: usize, z: usize, value: T);
 }
 
+/// Defines a mutable chunk.
 pub trait ChunkifyMut<T> {
   fn set(&mut self, x: usize, y: usize, z: usize, value: T);
 }
