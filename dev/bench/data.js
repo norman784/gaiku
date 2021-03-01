e60278f7fc480057cf168c7170df12ac051c4779
github-action-benchmark <github@users.noreply.github.com>
2021-03-01 18:54:21 +0000

    add Rust Benchmark (cargo) benchmark result for 8813a149b87be446b259888a1a5f583b01967f08

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614622177979,
+  "lastUpdate": 1614624861281,
   "repoUrl": "https://github.com/norman784/gaiku",
   "entries": {
     "Rust Benchmark": [
@@ -131,6 +131,138 @@ window.BENCHMARK_DATA = {
             "name": "voxel_terrain",
             "value": 142706148,
             "range": "± 1151980",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "normanpaniagua@gmail.com",
+            "name": "Norman Paniagua",
+            "username": "norman784"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8813a149b87be446b259888a1a5f583b01967f08",
+          "message": "Added bench workflow (#45)\n\n* Added bench workflow and use test::Benches instead of criterion\r\n* run and record bench only for master",
+          "timestamp": "2021-03-01T19:47:26+01:00",
+          "tree_id": "18ccb17f3b7a19caa4f5c34999a116db42df17ac",
+          "url": "https://github.com/norman784/gaiku/commit/8813a149b87be446b259888a1a5f583b01967f08"
+        },
+        "date": 1614624860899,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "heightmap_large_checkerboard",
+            "value": 782175,
+            "range": "± 34351",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "heightmap_medium_checkerboard",
+            "value": 53823,
+            "range": "± 2194",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "heightmap_planet",
+            "value": 7172115,
+            "range": "± 205044",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "heightmap_small_checkerboard",
+            "value": 1123,
+            "range": "± 124",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "heightmap_small_tree",
+            "value": 16214,
+            "range": "± 742",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "heightmap_terrain",
+            "value": 10062977,
+            "range": "± 410350",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "marching_cubes_large_checkerboard",
+            "value": 98158989,
+            "range": "± 3391046",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "marching_cubes_medium_checkerboard",
+            "value": 2838588,
+            "range": "± 104517",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "marching_cubes_planet",
+            "value": 26311086,
+            "range": "± 659545",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "marching_cubes_small_checkerboard",
+            "value": 19960,
+            "range": "± 1865",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "marching_cubes_small_tree",
+            "value": 867953,
+            "range": "± 20499",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "marching_cubes_terrain",
+            "value": 95254955,
+            "range": "± 1632145",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "voxel_large_checkerboard",
+            "value": 413714563,
+            "range": "± 4699582",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "voxel_medium_checkerboard",
+            "value": 4873696,
+            "range": "± 187711",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "voxel_planet",
+            "value": 55082199,
+            "range": "± 1506700",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "voxel_small_checkerboard",
+            "value": 137095,
+            "range": "± 8994",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "voxel_small_tree",
+            "value": 541031,
+            "range": "± 49055",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "voxel_terrain",
+            "value": 132819966,
+            "range": "± 3435673",
             "unit": "ns/iter"
           }
         ]
